cb80d3a8c1a69f3a81c334ced8f56551df886c5c
Eduardo Candido <38570289+EduardoCandido@users.noreply.github.com>
2021-04-12 22:28:20 -0300

    Using json-server for provide data and starting with HTTP in components

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { MEAT_API } from "app/app.api";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import { Restaurant } from "./restaurant/restaurant.model";
+
+@Injectable()
+export class RestaurantService{
+    constructor(private http:Http){}
+
+    restaurants(): Observable<Restaurant[]>{
+        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+    }
+}
