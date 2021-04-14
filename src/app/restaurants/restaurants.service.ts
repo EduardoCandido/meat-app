edb058d6299b974b7fc83b7e1c97e026fce300fc
Eduardo Candido <38570289+EduardoCandido@users.noreply.github.com>
2021-04-13 22:07:40 -0300

    Criando componente restaurant-detail

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,13 +3,17 @@ import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 import { Restaurant } from "./restaurant/restaurant.model";
+import { ErrorHandler } from "app/app.error-handler";
 
 @Injectable()
 export class RestaurantService{
     constructor(private http:Http){}
 
     restaurants(): Observable<Restaurant[]>{
-        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 }
