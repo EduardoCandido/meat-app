439ecf975d8e73a7332b0ea5fc377aad10bf1c00
Eduardo Candido <38570289+EduardoCandido@users.noreply.github.com>
2021-05-13 10:56:08 -0300

    Created component radio and using it in orderComponent

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html',
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[]= [
+    {label:"Cartão de crédito", value:"CC"},
+    {label:"Cartão de débito", value:"CD"},
+    {label:"Dinheiro", value:"D"},
+    {label:"Vale refeição", value:"VR"},
+  ]
 
   constructor() { }
 
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ContentChild, AfterContentInit } from '@angular/core';
+import {NgModel} from '@angular/forms';
 
 @Component({
-  selector: 'mt-input',
+  selector: 'mt-input-container',
   templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
-
+  @Input() label:string;
+  @Input() errorMessage:string;
+  @ContentChild(NgModel) model:NgModel
+  input:any;
+  
   ngOnInit() {
   }
 
+  ngAfterContentInit(){
+    this.input = this.model;
+    if(this.input === undefined){
+      throw new Error("Esse componente precisa ser usado com uma diretiva ngModel");      
+    }
+  }
+
+  hasSuccess():boolean{
+    return this.input.valid && (this.input.dirty || this.input.touched);
+  }
+
+  hasError():boolean{
+    return this.input.invalid && (this.input.dirty || this.input.touched);
+  }
 }
